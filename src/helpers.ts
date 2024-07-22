132013ad72ad29c82a6563914e504b8537b92a08
ihsangan <me@isan.eu.org>
2024-07-22 16:59:45 +0800

    markdown rewrite

--- src/helpers.ts
+++ src/helpers.ts
@@ -40,13 +40,59 @@ export function basicAuth(request: Request, env: Env): Response | null {
 }
 
 export function MDrewrite(str: string): string {
-  let markdownRegex = /\[([^\]]+)\]\(([^)]+)\)/g;
-  let urlOnlyRegex = /\[([^\]]+)\]/g;
-  str = str.replace(markdownRegex, (match, url, note) => {
-    return `<a href="${url}">${note.trim()}</a>`;
+  const markdownRegex = /\[([^\]]+)\]\(([^)]+)\)/g;
+  const urlOnlyRegex = /\[([^\]]+)\]/g;
+  const imageMarkdownRegex = /!\[([^\]]*)\]\(([^)]+)\)/g;
+  const headingRegex = /^(#{1,6})\s+(.*)$/gm;
+  const codeRegex = /```(\w+)\s*([\s\S]*?)```/g;
+  const inlineCodeRegex = /`([^`]+)`/g;
+  const boldRegex = /\*\*([^*]+)\*\*/g;
+  const italicRegex = /\*([^*]+)\*/g;
+  const newLineRegex = /(?:\r\n|\r|\n)/g;
+  const headingSizes = [
+    'xx-large',
+    'x-large',
+    'large',
+    'medium',
+    'small',
+    'x-small'
+  ];
+  let codeBlocks: { [key: string]: string } = {};
+  let codeBlockId = 0;
+  str = str.replace(codeRegex, (match, lang, code) => {
+    const id = `__CODE_BLOCK_${codeBlockId++}__`;
+    codeBlocks[id] = `<pre><code class="lang-${lang}" style="font-size:12px">${code.trim()}</code></pre>`;
+    return id;
+  });
+  str = str.replace(inlineCodeRegex, (match, code) => {
+    const id = `__INLINE_CODE_${codeBlockId++}__`;
+    codeBlocks[id] = `<code class="lang-none">${code.trim()}</code>`;
+    return id;
+  });
+  str = str.replace(boldRegex, (match, text) => {
+    return `<strong>${text.trim()}</strong>`;
+  });
+  str = str.replace(italicRegex, (match, text) => {
+    return `<em>${text.trim()}</em>`;
+  });
+  str = str.replace(imageMarkdownRegex, (match, altText, url) => {
+    return `<img src="${url.replace('://', '://i0.wp.com/').concat('?w=1280&h=1280&quality=85&ssl')}" alt="${altText.trim()}" loading="lazy">`;
+  });
+  str = str.replace(markdownRegex, (match, note, url) => {
+    return `<a href="${url}" target="_blank">${note.trim()}</a>`;
   });
   str = str.replace(urlOnlyRegex, (match, url) => {
-    return `<a href="${url}">${url}</a>`;
+    return `<a href="${url}" target="_blank">${url}</a>`;
   });
-  return str.replace(/(?:\r\n|\r|\n)/g, '<br/>');
+  str = str.replace(headingRegex, (match, hashes, text) => {
+    let level = hashes.length;
+    let fontSize = headingSizes[level - 1];
+    return `<div style="font-size:${fontSize}"><strong>${text.trim()}</strong></div>`;
+  });
+  str = str.replace(newLineRegex, '<br/>\n');
+
+  for (const id in codeBlocks) {
+    str = str.replace(id, codeBlocks[id]);
+  }
+  return str;
 }
